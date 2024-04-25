6aefd23093052e1bd5753253e0113bef2b36d904
MarcosBremermann <mabm2004@gmail.com>
2024-04-25 16:08:22 -0300

    Task 8 v1.0

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python function that lists all documents in a collection
+"""
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    """lists all documents in a collection"""
+
+    empty_list = []
+    if mongo_collection not NULL:
+        return mongo_collection.find()
+    return empty_list
