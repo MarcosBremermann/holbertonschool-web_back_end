e1cf95cb54dacea04aa458d5ef388db3a7640396
MarcosBremermann <mabm2004@gmail.com>
2024-04-26 02:42:59 -0300

    Task 8 v1.03

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -8,7 +8,6 @@ from pymongo import MongoClient
 def list_all(mongo_collection):
     """lists all documents in a collection"""
 
-    empty_list = []
-    if mongo_collection is not NULL:
-        return list(mongo_collection.find())
-    return empty_list
+    cursor = mongo_collection.find()
+    documents = list(cursor)
+    return documents
