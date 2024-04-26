c8a2e2341b250663435b3381a985999dab351925
MarcosBremermann <mabm2004@gmail.com>
2024-04-26 03:15:34 -0300

    Task 11 v1.0

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python function that returns the list of school having a specific topic
+"""
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function that returns the list of school having a specific topic
+    """
+
+    return mongo_collection.find({"topics": topic})
